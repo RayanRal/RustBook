2e134d6e859b5c847ef12ddaca7f6893c6aff24c
rayanral <rayanral@gmail.com>
2020-03-26 10:51:57 +0200

    rc and refcell

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,13 +1,15 @@
 use crate::List::{Cons, Nil};
 use std::ops::Deref;
 use std::mem;
+use std::rc::Rc;
 
 fn main() {
-    let list = Cons(1,
-                    Box::new(Cons(2,
-                                  Box::new(Cons(3,
-                                                Box::new(Nil))))));
+    smart_pointers();
+    custom_dropping();
+    rc_pointer();
+}
 
+fn smart_pointers() {
     let x = 5;
     let y = MyBox::new(x);
 
@@ -15,7 +17,9 @@ fn main() {
     assert_eq!(5, *y);
 
     hello(&MyBox::new(String::from("world")));
+}
 
+fn custom_dropping() {
     let c = CustomSmartPointer { data: String::from("my stuff") };
     let d = CustomSmartPointer { data: String::from("other stuff") };
     println!("custom pointers created");
@@ -23,12 +27,26 @@ fn main() {
     println!("custom pointers dropped before end");
 }
 
+fn rc_pointer() {
+    let a = Rc::new(Cons(5,
+                         Rc::new(Cons(10,
+                            Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after dropping c = {}", Rc::strong_count(&a));
+}
+
 fn hello(name: &str) {
     println!("Hello, {}!", name);
 }
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
@@ -49,7 +67,7 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
-
+//custom dropping functions with Drop trait
 struct CustomSmartPointer {
     data: String,
 }
