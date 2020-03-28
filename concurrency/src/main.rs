928d7e3376d213d8f0721a263260d91c4f344341
rayanral <rayanral@gmail.com>
2020-03-28 15:01:37 +0200

    adding object-oriented chapter

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::time::Duration;
-use std::sync::mpsc;
+use std::sync::{mpsc, Mutex, Arc};
+use std::rc::Rc;
 
 
 fn main() {
@@ -56,5 +57,21 @@ fn channelling() {
 }
 
 fn shared_state() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle)
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
